9774bcb7a6356f5f3138dde7695f7df6a72d3bac
86289Sahil <42300607+86289Sahil@users.noreply.github.com>
2018-10-15 21:58:55 +0530

    Add files via upload

---
+++ CSVFile.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+class CSVfile 
+{
+     public static void main(String[] args) throws IOException
+	{      
+         String fileInput = "C://Users//sahil//Desktop//hey.txt";
+        FileWriter f1=new FileWriter(fileInput);
+        String str=("shivay,shivam,piyush");
+        f1.write(str);
+		f1.close();
+		FileReader f2=new FileReader(fileInput);
+        int ch;
+        while ((ch = f2.read()) != -1) 
+		{
+            System.out.print((char)ch);
+		}
+         f2.close();
+           
+    }
+}
